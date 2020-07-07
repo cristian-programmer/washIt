f14320a1115802f39964a760bb25e3f01dfedf9c
Cristian Vargas <vargascristian@americana.edu.co>
2020-07-07 02:59:36 -0500

    generate project in react-native

---
+++ frontend/App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const App = () =>{
+  return(
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
+    <Text>
+      Hello word!! :) :)
+    </Text>
+  </View>)
+}
+
+export default App;
